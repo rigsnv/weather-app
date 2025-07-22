d7a2475ade07b8c5075d9e8a2d985a1a6440d824
rigsnv <rigsnv@gmail.com>
2025-07-22 17:51:16 +0100

    Fixing issues #7 and #8

--- src/components/ui/WeatherIcon.jsx
+++ src/components/ui/WeatherIcon.jsx
@@ -1,3 +1,5 @@
+import { HelpCircle } from 'lucide-react';
+
 const WeatherIcon = ({ weatherCodeMap, code, className = '', ...props }) => {
     const { icon: IconComponent, color } = weatherCodeMap[code] || { icon: HelpCircle, color: 'text-gray-400' };
     return <IconComponent className={`${color} ${className}`} {...props} />
